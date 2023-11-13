1533519e3a6fc217a5602f7fd43613a700642c23
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-12 22:50:04 -0800

    create operate function to handle calc method and output display

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const calc = {
     divide: (a, b) => a/b,
 }
 
+let output = 0
 let operationType
 let firstNumber
 let secondNumber
 
+const outputEl = document.querySelector('.output')
+outputEl.innerHTML = output
 document.querySelectorAll('.op').forEach(el => {
     el.addEventListener('click', e => {
         const {op} = el.dataset
         console.log(op)
     })
 })
+
+const operate = (op, a, b) => {
+    output = calc[op](a, b)
+    outputEl.innerHTML = output
+}
