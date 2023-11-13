fb70c03b6f4e46211fef8d95e33e68871e4e9073
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-12 22:20:40 -0800

    create basic operation functions

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+const calc = {
+    add: (a, b) => a+b,
+    subtract: (a, b) => a-b,
+    multiply: (a, b) => a*b,
+    divide: (a, b) => a/b,
+}
+
+
 const outputEl = document.querySelector('.output')
 outputEl.innerText = 0
